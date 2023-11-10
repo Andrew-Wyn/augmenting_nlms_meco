f648e0371ea34b1863d3713859cb4ba2db875ee4
Luca Dini <luca.dini16@gmail.com>
2023-11-10 16:48:29 +0100

    added trainer for multitask binary classification training

---
+++ anm/gaze_training/multitask_trainer.py
@@ -0,0 +1,76 @@
+from typing import Dict, Union, Any, Optional, Tuple, List
+from transformers.trainer_pt_utils import nested_detach
+from transformers import Trainer
+from torch import nn
+import inspect
+import torch
+
+
+
+
+class MultitaskTrainer(Trainer):
+
+    def _set_signature_columns_if_needed(self):
+        if self._signature_columns is None:
+            # Inspect model forward signature to keep only the arguments it accepts.
+            signature = inspect.signature(self.model.forward)
+            self._signature_columns = list(signature.parameters.keys())
+            # Labels may be named label or label_ids, the default data collator handles that.
+            self._signature_columns += list(set(["label", "label_ids", "label_pos", "label_neg"] + self.label_names))
+
+    def prediction_step(
+            self,
+            model: nn.Module,
+            inputs: Dict[str, Union[torch.Tensor, Any]],
+            prediction_loss_only: bool,
+            ignore_keys: Optional[List[str]] = None,
+    ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
+
+        has_labels = False if len(self.label_names) == 0 else all(
+            inputs.get('labels')[k] is not None for k in self.label_names)
+
+        return_loss = inputs.get("return_loss", None)
+        if return_loss is None:
+            return_loss = self.can_return_loss
+        loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
+
+        inputs = self._prepare_inputs(inputs)
+        if ignore_keys is None:
+            if hasattr(self.model, "config"):
+                ignore_keys = getattr(self.model.config, "keys_to_ignore_at_inference", [])
+            else:
+                ignore_keys = []
+
+        # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
+        if has_labels or loss_without_labels:
+            labels = nested_detach(inputs['labels'])
+            # labels = nested_detach(tuple(inputs.get('labels')[name] for name in self.label_names))
+            if len(labels) == 1:
+                labels = labels[0]
+        else:
+            labels = None
+
+        with torch.no_grad():
+            if has_labels or loss_without_labels:
+                with self.compute_loss_context_manager():
+                    loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
+                loss = loss.mean().detach()
+                logits = outputs['logits']
+
+            else:
+                loss = None
+                with self.compute_loss_context_manager():
+                    outputs = model(**inputs)
+                logits = outputs['logits']
+                # TODO: this needs to be fixed and made cleaner later.
+                if self.args.past_index >= 0:
+                    self._past = outputs[self.args.past_index - 1]
+
+        if prediction_loss_only:
+            return (loss, None, None)
+
+        logits = nested_detach(logits)
+        if len(logits) == 1:
+            logits = logits[0]
+
+        return (loss, logits, labels)
